64fb8a4e9f5ba8dd1d43e59f35c2c2532445681a
Andrej Kenda <andrej.baca14@gmail.com>
2023-02-23 20:20:48 +0100

    Replace passwords with tokens
    - replace passwords with tokens
    - update gogs api auth in requests
    - switch to using fstrings

--- setup_mirrors.py
+++ setup_mirrors.py
@@ -20,11 +20,11 @@ def next_link(response):
         return None
 
 
-def fetch_repos(user, password):
+def fetch_repos(user, token):
     repos = []
     next_url = REPOS_URL
     while True:
-        response = requests.get(next_url, auth=(user, password))
+        response = requests.get(next_url, auth=(user, token))
         repos = repos + response.json()
 
         next_url = next_link(response)
@@ -34,23 +34,25 @@ def fetch_repos(user, password):
 
 
 class Gogs:
-    def __init__(self, base_url, user, password):
+    def __init__(self, base_url, user, token):
         self.base_url = base_url
         self.user = user
-        self.password = password
+        self.token = token
 
     def user_id(self):
-        url = "{}/api/v1/user".format(self.base_url)
-        response = requests.get(url, auth=(self.user, self.password))
+        url = f"{self.base_url}/api/v1/user"
+        params = {"token": self.token}
+        response = requests.get(url, params=params)
         return response.json()["id"]
 
     def org_id(self, org_name):
-        url = "{}/api/v1/orgs/{}".format(self.base_url, org_name)
-        response = requests.get(url, auth=(self.user, self.password))
+        url = f"{self.base_url}/api/v1/orgs/{org_name}"
+        params = {"token": self.token}
+        response = requests.get(url, params=params)
         return response.json()["id"]
 
     def mirror(self, owner_id, repo):
-        url = "{}/api/v1/repos/migrate".format(self.base_url)
+        url = f"{self.base_url}/api/v1/repos/migrate"
         body = {
             "clone_addr": repo["clone_url"],
             "uid": owner_id,
@@ -60,35 +62,36 @@ class Gogs:
             "description": repo["description"]
         }
 
-        return requests.post(url, json=body, auth=(self.user, self.password))
+        params = {"token": self.token}
+        return requests.post(url, json=body, params=params)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--gh-user", required=True, help="github user")
-    parser.add_argument("--gh-pass", help="github password")
+    parser.add_argument("--gh-token", help="github token")
     parser.add_argument("--gogs-url", required=True, help="gogs server URL")
     parser.add_argument("--gogs-user", required=True, help="gogs user")
-    parser.add_argument("--gogs-pass", help="gogs password")
+    parser.add_argument("--gogs-token", help="gogs token")
     parser.add_argument("--gogs-org", help="Gogs organization to mirror to")
     parser.add_argument("--with-forks", type=bool, help="Mirror forks")
     args = parser.parse_args()
 
     gh_user = args.gh_user
-    gh_pass = args.gh_pass
+    gh_token = args.gh_token
     gogs_url = args.gogs_url
     gogs_user = args.gogs_user
-    gogs_pass = args.gogs_pass
+    gogs_token = args.gogs_token
     gogs_org = args.gogs_org
     with_forks = args.with_forks
 
-    if not gh_pass:
-        gh_pass = input("Github password: ")
-    if not gogs_pass:
-        gogs_pass = input("Gogs password: ")
+    if not gh_token:
+        gh_token = input("Github token: ")
+    if not gogs_token:
+        gogs_token = input("Gogs token: ")
 
     # Load repositories
-    repos = fetch_repos(gh_user, gh_pass)
+    repos = fetch_repos(gh_user, gh_token)
 
     # Filter out repos from orgs that the user belongs to
     repos = [r for r in repos if r["owner"]["login"] == gh_user]
@@ -97,25 +100,24 @@ def main():
     if not with_forks:
         repos = [r for r in repos if not r["fork"]]
 
-    gogs = Gogs(gogs_url, gogs_user, gogs_pass)
+    gogs = Gogs(gogs_url, gogs_user, gogs_token)
 
     if gogs_org:
-        print("Mirror to organization {}".format(gogs_org))
+        print(f"Mirror to organization {gogs_org}")
         gogs_id = gogs.org_id(gogs_org)
     else:
-        print("Mirror to user {}".format(gogs_user))
+        print(f"Mirror to user {gogs_user}")
         gogs_id = gogs.user_id()
 
     # Set up the mirrors
     for repo in repos:
         response = gogs.mirror(gogs_id, repo)
         if response.status_code == 201:
-            print("Mirror for {} set up".format(repo["name"]))
+            print(f"Mirror for {repo['name']} set up")
         elif response.status_code == 500:
-            print("Repository {} already exists".format(repo["name"]))
+            print(f"Repository {repo['name']} already exists")
         else:
-            print("Unknown error {} for repo {}".format(
-                response.status_code, repo["name"]))
+            print(f"Unknown error {response.status_code} for repo {repo['name']}")
 
 
 if __name__ == "__main__":
